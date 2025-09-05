c06113781ceb2861205bf1578ed834335f2e8282
Asalaliya Taksh Kantibhai <takshasalaliya@gmail.com>
2025-09-05 14:56:33 +0530

    Update RegistrationForm.jsx

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1655,7 +1655,10 @@
                           </p>
                         )}
                         
-                       
+                      </div>
+                    </div>
+                  )}
+
                   {/* Transaction Details */}
                   <div className="space-y-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
